52e72d302ee7c28ab449422f5256386946ba65f7
Isaiah Francois <francoisisaiaht@yahoo.com>
2019-06-19 12:48:48 -0400

    Typo on line 29

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -26,7 +26,7 @@ class TabLink {
 
         // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
         Array.from(links).forEach(link => {
-            return links.classList.remove('tabs-link-selected')
+            return link.classList.remove('tabs-link-selected')
         });
 
         // Add a class named "tabs-link-selected" to this link
