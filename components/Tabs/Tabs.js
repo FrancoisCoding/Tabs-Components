97a13e9294ef1120deb68a20bfdf7ddb612b205d
Isaiah Francois <francoisisaiaht@yahoo.com>
2019-06-19 00:44:32 -0400

    Added to TabLink Class

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -2,23 +2,30 @@ class TabLink {
     constructor(element) {
         // Assign this.element to the passed in DOM element
         // this.element;
-
+        this.element = element;
+        console.log(this.element);
         // Get the custom data attribute on the Link
-        // this.data;
+        this.data = this.element.dataset;
+        console.log(this.data);
 
         // Using the custom data attribute get the associated Item element
-        // this.itemElement;
+        this.itemElement = this.data.tab;
+        console.log(this.itemElement);
 
         // Using the Item element, create a new instance of the TabItem class
-        // this.tabItem;
+        this.tabItem = new TabItem(this.itemElement);
+        console.log(this.tabItem);
 
         // Add a click event listener on this instance, calling the select method on click
-
+        this.tabItem.onclick = function () {
+            this.select();
+        }
     };
 
     select() {
         // Get all of the elements with the tabs-link class
-        // const links;
+        const links = this.element;
+        console.log(links)
 
         // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
         // Array.from(links).forEach();
@@ -58,4 +65,7 @@ class TabItem {
 
 */
 
-//links = document.querySelectorAll();
+links = document.querySelectorAll(".tabs-link");
+links.forEach(link => {
+    return new TabLink(link);
+})
